5235a7c50dde5c84ff93d2e2f48242026ee62fca
Radek12345 <gosciminskiradoslaw@gmail.com>
2018-10-12 18:59:42 +0200

    Update Update

--- vega/Persistence/VegaDbContext.cs
+++ vega/Persistence/VegaDbContext.cs
@@ -7,6 +7,7 @@ namespace vega.Persistence
     {
         public DbSet<Make> Makes { get; set; }
         public DbSet<Feature> Features { get; set; }
+        public DbSet<Vehicle> Vehicle { get; set; }
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
             : base(options)
         {
