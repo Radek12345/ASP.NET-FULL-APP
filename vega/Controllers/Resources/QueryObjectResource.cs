36e6c7c962a2858175abf6831ab941f9fe0a87bb
Radek12345 <gosciminskiradoslaw@gmail.com>
2018-11-01 14:32:50 +0100

    Small refactor

---
+++ vega/Controllers/Resources/QueryObjectResource.cs
@@ -0,0 +1,8 @@
+namespace vega.Controllers.Resources
+{
+    public class QueryObjectResource
+    {
+        public string SortBy { get; set; }
+        public bool IsSortAscending { get; set; }
+    }
+}
--- vega/Controllers/Resources/VehicleQueryResource.cs
+++ vega/Controllers/Resources/VehicleQueryResource.cs
@@ -1,10 +1,8 @@
 namespace vega.Controllers.Resources
 {
-    public class VehicleQueryResource
+    public class VehicleQueryResource : QueryObjectResource
     {
         public int? MakeId { get; set; }
         public int? ModelId { get; set; }
-        public string SortBy { get; set; }
-        public bool IsSortAscending { get; set; }
     }
 } 
